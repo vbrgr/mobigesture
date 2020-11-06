46743f3ee7c7dec315d861be685aafa14778b92f
govindrp <govinda@rpwebapps.in>
2020-11-06 17:33:35 +0530

    first

--- script.js
+++ script.js
@@ -1,47 +1,51 @@
 var selectedRow = null
-fetch();
+
 function onFormSubmit() {
-    if (validate()) {
         var formData = readFormData();
         if (selectedRow == null)
             insertNewRecord(formData);
         else
             updateRecord(formData);
         resetForm();
-    }
+    
 }
 
 function readFormData() {
     var formData = {};
-    formData["fullName"] = document.getElementById("fullName").value;
-    formData["empCode"] = document.getElementById("empCode").value;
-    formData["salary"] = document.getElementById("salary").value;
-    formData["city"] = document.getElementById("city").value;
+    formData["userid"] = document.getElementById("userid").value;
+    formData["username"] = document.getElementById("username").value;
+    formData["email"] = document.getElementById("email").value;
     return formData;
 }
-
+//fetch();
 function insertNewRecord(data) {
-    var table = document.getElementById("employeeList").getElementsByTagName('tbody')[0];
+    var table = document.getElementById("table").getElementsByTagName('tbody')[0];
     var newRow = table.insertRow(table.length);
+    fetch('https://jsonplaceholder.typicode.com/posts', {
+  method: 'POST',
+  body: JSON.stringify(data),
+  headers: {
+    'Content-type': 'application/json; charset=UTF-8',
+  },
+})
+  .then((response) => response.json())
+  .then((json) => console.log(json))
     cell1 = newRow.insertCell(0);
-    cell1.innerHTML = data.fullName;
+    cell1.innerHTML = data.userid;
     cell2 = newRow.insertCell(1);
-    cell2.innerHTML = data.empCode;
+    cell2.innerHTML = data.username;
     cell3 = newRow.insertCell(2);
-    cell3.innerHTML = data.salary;
+    cell3.innerHTML = data.email;
     cell4 = newRow.insertCell(3);
-    cell4.innerHTML = data.city;
-    cell4 = newRow.insertCell(4);
     cell4.innerHTML = `<a onClick="onEdit(this)">Edit</a>
                        <a onClick="onDelete(this)">Delete</a>`;
 }
 function fetch(){
 var table = document.getElementById("table").getElementsByTagName('tbody')[0];
     var newRow = table.insertRow(table.length);
-    /* fetch('https://my-json-server.typicode.com/vbrgr/mobigesture/users').then((response) => response.json())
-    .then((json) => { 
-        console.log(json);
-        /* for (let index = 0; index < json.length; index++) {
+    fetch('https://my-json-server.typicode.com/vbrgr/mobigesture/users')
+    .then((response) => response.json())
+    .then((json) => { for (let index = 0; index < json.length; index++) {
         const element = json[index];        
     cell1 = newRow.insertCell(0);
     cell1.innerHTML = element.userid;
@@ -52,51 +56,34 @@ var table = document.getElementById("table").getElementsByTagName('tbody')[0];
     cell4 = newRow.insertCell(3);
     cell4.innerHTML = `<a onClick="onEdit(this)">Edit</a>
                        <a onClick="onDelete(this)">Delete</a>`;
-    } *
-    }) */
-    fetch('https://my-json-server.typicode.com/vbrgr/mobigesture/users')
-  .then((response) => response.json())
-  .then((json) => console.log(json))
+    }
+    })
     
 }
 function resetForm() {
-    document.getElementById("fullName").value = "";
-    document.getElementById("empCode").value = "";
-    document.getElementById("salary").value = "";
-    document.getElementById("city").value = "";
+    document.getElementById("userid").value = "";
+    document.getElementById("username").value = "";
+    document.getElementById("email").value = "";
     selectedRow = null;
 }
 
 function onEdit(td) {
     selectedRow = td.parentElement.parentElement;
-    document.getElementById("fullName").value = selectedRow.cells[0].innerHTML;
-    document.getElementById("empCode").value = selectedRow.cells[1].innerHTML;
-    document.getElementById("salary").value = selectedRow.cells[2].innerHTML;
-    document.getElementById("city").value = selectedRow.cells[3].innerHTML;
+    document.getElementById("userid").value = selectedRow.cells[0].innerHTML;
+    document.getElementById("username").value = selectedRow.cells[1].innerHTML;
+    document.getElementById("email").value = selectedRow.cells[2].innerHTML;
 }
 function updateRecord(formData) {
-    selectedRow.cells[0].innerHTML = formData.fullName;
-    selectedRow.cells[1].innerHTML = formData.empCode;
-    selectedRow.cells[2].innerHTML = formData.salary;
-    selectedRow.cells[3].innerHTML = formData.city;
+    selectedRow.cells[0].innerHTML = formData.userid;
+    selectedRow.cells[1].innerHTML = formData.username;
+    selectedRow.cells[2].innerHTML = formData.email;
 }
 
 function onDelete(td) {
     if (confirm('Are you sure to delete this record ?')) {
         row = td.parentElement.parentElement;
-        document.getElementById("employeeList").deleteRow(row.rowIndex);
+        document.getElementById("table").deleteRow(row.rowIndex);
         resetForm();
     }
 }
-function validate() {
-    isValid = true;
-    if (document.getElementById("fullName").value == "") {
-        isValid = false;
-        document.getElementById("fullNameValidationError").classList.remove("hide");
-    } else {
-        isValid = true;
-        if (!document.getElementById("fullNameValidationError").classList.contains("hide"))
-            document.getElementById("fullNameValidationError").classList.add("hide");
-    }
-    return isValid;
-}
+
